35ad0118dc4ab87e898d42391d8f468d8377c443
michael-iden <michael.iden@mybuys.com>
2015-07-20 17:33:22 -0400

    Making the scraper a class so we can pass around the resulting player list. Removing unused class

--- Scraper/FanDuelScraper.py
+++ Scraper/FanDuelScraper.py
@@ -7,39 +7,39 @@ import urllib2
 import re
 import json
 
-fan_duel_game_url = 'https://www.fanduel.com/e/Game/12662'
+class FanDuelScraper:
+
+    def __init__(self, fan_duel_game_url):
+        self.fan_duel_game_url = fan_duel_game_url
+
+    def get_fan_duel_players(self):
+        webpage = urllib2.urlopen(self.fan_duel_game_url)
+        soup = BeautifulSoup(webpage)
+        script = soup.find('script', text=re.compile('FD\.playerpicker\.allPlayersFullData'))
+
+        json_text = re.search(r'^\s*FD\.playerpicker\.allPlayersFullData\s*=\s*({.*?})\s*;\s*$',
+                              script.string, flags=re.DOTALL | re.MULTILINE).group(1)
+
+        data = json.loads(json_text)
 
 
-webpage = urllib2.urlopen(fan_duel_game_url)
-soup = BeautifulSoup(webpage)
-script = soup.find('script', text=re.compile('FD\.playerpicker\.allPlayersFullData'))
-
-json_text = re.search(r'^\s*FD\.playerpicker\.allPlayersFullData\s*=\s*({.*?})\s*;\s*$',
-                      script.string, flags=re.DOTALL | re.MULTILINE).group(1)
-
-data = json.loads(json_text)
+        print json.dumps(data,indent=1)
 
 
-print json.dumps(data,indent=1)
+        fan_duel_players = list()
+        for fan_duel_id in data.keys():
 
+            position = data[fan_duel_id][0]
+            name = data[fan_duel_id][1]
+            team_number = data[fan_duel_id][2]
+            fan_duel_cost = data[fan_duel_id][5]
+            fan_duel_fppg = data[fan_duel_id][6]
 
-fan_duel_players = list()
-for fan_duel_id in data.keys():
+            player = Player(name, position, team_number, fan_duel_id, fan_duel_cost, fan_duel_fppg)
 
-    position = data[fan_duel_id][0]
-    name = data[fan_duel_id][1]
-    team_number = data[fan_duel_id][2]
-    fan_duel_cost = data[fan_duel_id][5]
-    fan_duel_fppg = data[fan_duel_id][6]
+            fan_duel_players.append(player)
 
-    player = Player(name, position, team_number, fan_duel_id, fan_duel_cost, fan_duel_fppg)
-
-    fan_duel_players.append(player)
-
-print fan_duel_players.__sizeof__()
-
-for player in fan_duel_players:
-    player.print_player()
+        return fan_duel_players
 
 
 
