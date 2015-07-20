f36060ae547dd2776948d5178ccd09ae38c38a4c
michael-iden <michael.iden@mybuys.com>
2015-07-20 16:43:42 -0400

    Parsing the JSON and putting the values into a player class. Got rid of many of the unnecessary classes.

--- Scraper/FanDuelScraper.py
+++ Scraper/FanDuelScraper.py
@@ -7,30 +7,9 @@ from bs4 import BeautifulSoup
 import urllib2
 import re
 import json
-import base64
-import requests
-import cookielib
-
-username = 'michaeliden90@gmail.com'
-password = 'igQpbdE3GtouQNG8h'
-
-fanduelurl = 'https://www.fanduel.com/c/CCAuth'
 
 fan_duel_game_url = 'https://www.fanduel.com/e/Game/12662'
 
-# data= {
-#     'cc_session_id': 'qleqcfn08cmcq5tldjhn8f0if3',
-#     'cc_action': 'cca_login',
-#     'cc_failure_url': 'https://www.fanduel.com/p/LoginPp',
-#     'cc_success_url': 'https://www.fanduel.com/',
-#     'email': username,
-#     'password': password,
-#     'login': 'Log in',
-#     'checkbox_remember': '1'
-#     }
-#
-#
-# print requests.get(fanduelurl, data=data).text
 
 webpage = urllib2.urlopen(fan_duel_game_url)
 soup = BeautifulSoup(webpage)
@@ -43,8 +22,6 @@ data = json.loads(json_text)
 
 
 # print json.dumps(data,indent=1)
-
-# print data
 
 
 fan_duel_players = list()
