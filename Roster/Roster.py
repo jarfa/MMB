3862678724ec21028d6a3a269a6757ed575370e7
jarfa <jonathanarfa@gmail.com>
2015-07-20 16:48:31 -0400

    first few methods of Roster class

--- Roster/Roster.py
+++ Roster/Roster.py
@@ -1,5 +1,17 @@
 __author__ = 'Mike'
 
 class Roster:
+    def __init__(self, limits = {
+        'P':1, 'C':1, '1B':1, '2B': 1, '3B': 1, 'SS': 1, 'OF': 3
+        }):
+        self.limits = limits
+        self.allocated = dict((k,0) for k  in limits.keys())
+        self.player_list = []
 
-    def getTeam
+    def add(player):
+        if self.allocated[player.position] + 1 > self.limits[player.position]:
+            raise Exception(
+                "Can't add, already have enough players at position %s (limit %d)" %
+                (player.position, self.limits[player.position]))
+        self.allocated[player.position] += 1
+        self.player_list.append(player)
