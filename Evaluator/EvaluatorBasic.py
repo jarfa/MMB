ff7274e58ce42580138e8d08519d57bbb65f53dc
Kevin Hanlon <kevin.hanlon@mybuys.com>
2015-07-21 10:33:08 -0400

    got the evaluator scraper to get batter stats

--- Evaluator/EvaluatorBasic.py
+++ Evaluator/EvaluatorBasic.py
@@ -4,9 +4,3 @@ from Player.Player import Player
 class EvaluatorBasic(Evaluator.Evaluator):
 	def getPlayerValue(self, player):
 		return player.getFPPG()
-		
-e = EvaluatorBasic()
-#Player(name, position, team_number, fan_duel_id, fan_duel_cost, fan_duel_fppg)
-player = Player('test name', 'p', '1', '1', '1000', '3.2538')
-score = e.getPlayerValue(player)
-print 'Final score: ', score
